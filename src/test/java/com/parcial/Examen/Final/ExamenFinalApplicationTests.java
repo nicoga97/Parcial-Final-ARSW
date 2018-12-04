47a829bdbe1eb0a632a60d6479adcb1d8abdc3fc
nicoga97 <nicoga97@gmail.com>
2018-12-04 11:57:23 -0500

    heroku problem

--- src/test/java/com/parcial/Examen/Final/ExamenFinalApplicationTests.java
+++ src/test/java/com/parcial/Examen/Final/ExamenFinalApplicationTests.java
@@ -1,12 +1,12 @@
 package com.parcial.Examen.Final;
 
-import com.sun.deploy.net.HttpResponse;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
-import sun.net.www.http.HttpClient;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
